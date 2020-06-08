881a6c84b6f14e9b47592e3732968e30d0abec0b
GreyHuhu <1519715742@qq.com>
2020-06-08 10:39:02 +0800

    使用分支来进行版本控制

--- src/api/index.js
+++ src/api/index.js
@@ -3,16 +3,30 @@ const api = {
   Login: URL_PREFIX + '/users/login',
   LoginByPhone: URL_PREFIX + "/users/login-by-phone",
   Logout: URL_PREFIX + '/users/logout',
-  ForgePassword: '/auth/forge-password',
+  GetAllUser: URL_PREFIX + '/users/get-all-users',
+  GetCurrentUser: URL_PREFIX + '/users/get-current-user',
   Register: URL_PREFIX + '/users/register',
+  ForgePassword: '/auth/forge-password',
   SendSms: URL_PREFIX + '/phone/get-phone-code',
   CompareSms: URL_PREFIX + '/phone/compare-code',
   UserInfo: URL_PREFIX + '/users/get-current-user',
-  GetAllUser: URL_PREFIX + '/users/get-all-users',
-  GetAllNews: URL_PREFIX + '/news/get-all-news',
-  GetAllComments: URL_PREFIX + '/comments/get-all-comments',
-  GetTheComments: URL_PREFIX + '/comments/get-the-comments',
-  DeleteTheComments: URL_PREFIX + '/comments/delete-the-comments',
-  InsertTheComments: URL_PREFIX + '/comments/insert-the-comments',
 }
-export default api
+const reading = {
+  GetAllReadingGroup: URL_PREFIX + "/reading/get-all-groups",
+  SearchGroupsByTitle: URL_PREFIX + "/reading/search-title",
+  GetCurrentReadings: URL_PREFIX + "/reading/get-contents",
+  GetCurrentReadingQuestions: URL_PREFIX + "/reading/get-questions",
+  CompareGroupAnswers: URL_PREFIX + "/reading/compare-answers",
+  SearchGroup: URL_PREFIX + "/reading/search-title",
+  DeleteGroup: URL_PREFIX + "/reading/delete-group",
+  InsertHistory: URL_PREFIX + "/reading/insert-history",
+}
+
+const writing = {
+  GetAllCompositions: URL_PREFIX + "/writing/get-all-compositions"
+}
+export {
+  api,
+  reading,
+  writing
+}
--- src/config/router.config.js
+++ src/config/router.config.js
@@ -25,7 +25,7 @@ export const asyncRouterMap = [
           {
             path: '/news/reading',
             name: 'news_reading',
-            component: () => import('@/views/news/news'),
+            component: () => import('@/views/dashboard/Analysis'),
             meta: {
               title: '英语新闻',
               keepAlive: true,
@@ -34,7 +34,7 @@ export const asyncRouterMap = [
           {
             path: '/news/record',
             name: 'news_record',
-            component: () => import('@/views/news/comments'),
+            component: () => import('@/views/dashboard/Workplace'),
             meta: {
               title: '我的评论',
               keepAlive: true,
@@ -96,7 +96,7 @@ export const asyncRouterMap = [
           {
             path: '/writing/composition_bank',
             name: 'composition_bank',
-            component: () => import('@/views/list/TableList'),
+            component: () => import('@/views/writing/WritingBank'),
             meta: {
               title: '作文题库',
               keepAlive: true
@@ -105,19 +105,30 @@ export const asyncRouterMap = [
           {
             path: '/writing/my_composition',
             name: 'my_composition',
-            component: () => import('@/views/list/StandardList'),
+            component: () => import('@/views/writing/MyWriting'),
             meta: {
               title: '我的作文',
               keepAlive: true
             }
-          }]
+          },
+          {
+            path: '/writing/composition_bank/write',
+            name: 'write',
+            component: () => import('@/views/writing/Writing'),
+            hidden: true,
+            meta: {
+              title: '写作文',
+              keepAlive: true
+            }
+          }
+        ]
       },
 
       // 阅读理解
       {
         path: '/reading',
         name: 'reading',
-        component: RouteView,
+        component: PageView,
         redirect: '/reading/reading_groups',
         meta: {
           title: '阅读理解',
@@ -127,16 +138,36 @@ export const asyncRouterMap = [
           {
             path: '/reading/reading_groups',
             name: 'reading_groups',
-            component: () => import('@/views/profile/basic/Index'),
+            component: () => import('@/views/reading/ReadingGroup'),
             meta: {
               title: '阅读练习',
               keepAlive: true,
             }
           },
           {
+            path: "/reading/reading_groups/reading",
+            name: "reading_content",
+            hidden: true,
+            component: () => import('@/views/reading/Reading'),
+            meta: {
+              title: '阅读文章',
+              keepAlive: true,
+            }
+          },
+          {
+            path: "/reading/reading_groups/result",
+            name: "reading_result",
+            hidden: true,
+            component: () => import('@/views/reading/ReadingSuccess'),
+            meta: {
+              title: '提交成功',
+              keepAlive: true,
+            }
+          },
+          {
             path: '/reading/reading_lists',
             name: 'reading_lists',
-            component: () => import('@/views/profile/advanced/Advanced'),
+            component: () => import('@/views/reading/ReadingList'),
             meta: {
               title: '阅读记录',
               keepAlive: true,
@@ -197,19 +228,19 @@ export const asyncRouterMap = [
         hidden: true,
         component: PageView,
         redirect: '/result/success',
-        meta: {title: '结果页', icon: 'check-circle-o', permission: ['result']},
+        meta: {title: '结果页', icon: 'check-circle-o'},
         children: [
           {
             path: '/result/success',
             name: 'ResultSuccess',
             component: () => import(/* webpackChunkName: "result" */ '@/views/result/Success'),
-            meta: {title: '成功', keepAlive: false, hiddenHeaderContent: true, permission: ['result']}
+            meta: {title: '成功', keepAlive: false, hiddenHeaderContent: true}
           },
           {
             path: '/result/fail',
             name: 'ResultFail',
             component: () => import(/* webpackChunkName: "result" */ '@/views/result/Error'),
-            meta: {title: '失败', keepAlive: false, hiddenHeaderContent: true, permission: ['result']}
+            meta: {title: '失败', keepAlive: false, hiddenHeaderContent: true}
           }
         ]
       },
@@ -221,25 +252,25 @@ export const asyncRouterMap = [
         component: RouteView,
         hidden: true,
         redirect: '/exception/403',
-        meta: {title: '异常页', icon: 'warning', permission: ['exception']},
+        meta: {title: '异常页', icon: 'warning'},
         children: [
           {
             path: '/exception/403',
             name: 'Exception403',
             component: () => import(/* webpackChunkName: "fail" */ '@/views/exception/403'),
-            meta: {title: '403', permission: ['exception']}
+            meta: {title: '403'}
           },
           {
             path: '/exception/404',
             name: 'Exception404',
             component: () => import(/* webpackChunkName: "fail" */ '@/views/exception/404'),
-            meta: {title: '404', permission: ['exception']}
+            meta: {title: '404'}
           },
           {
             path: '/exception/500',
             name: 'Exception500',
             component: () => import(/* webpackChunkName: "fail" */ '@/views/exception/500'),
-            meta: {title: '500', permission: ['exception']}
+            meta: {title: '500'}
           }
         ]
       }
