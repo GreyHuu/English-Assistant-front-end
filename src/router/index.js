7cbc927a09fb01862995e94c5d087db8ca641560
GreyHuhu <1519715742@qq.com>
2020-06-16 13:06:49 +0800

    打包部署

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,12 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import { constantRouterMap, asyncRouterMap } from '@/config/router.config'
+import {constantRouterMap, asyncRouterMap} from '@/config/router.config'
 
 Vue.use(Router)
 
 export default new Router({
-  mode: 'history',
+  mode: 'hash',
   base: process.env.BASE_URL,
-  scrollBehavior: () => ({ y: 0 }),
+  scrollBehavior: () => ({y: 0}),
   routes: constantRouterMap.concat(asyncRouterMap)
 })
